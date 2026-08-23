5596aa601196343d45e338cf04c2ef8bb4af80be
Pavel <paveljevstignejev@gmail.com>
2026-08-23 06:20:27 +0300

    chore(fonts): self-host Manrope and IBM Plex Mono instead of Google Fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource-variable/manrope";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/ibm-plex-mono/600.css";
 import App from "./App";
 import "./styles.css";
 
